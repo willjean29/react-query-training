ea19ead59f0612fdbc12ed705ca8fb63fe4d4992
Jean Osco <willjean29@gmail.com>
2023-12-30 15:20:56 -0500

    feat(issues): change fetch to axios and create github api file

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,19 @@
-
+import { useQuery } from "@tanstack/react-query";
+import { githubApi } from "../../api/github.api";
+import { Label } from "../interfaces/label";
+const getLabels = async (): Promise<Label[]> => {
+  const { data } = await githubApi.get<Label[]>("/labels");
+  return data;
+};
 export const LabelPicker = () => {
+  const labelsQuery = useQuery(["todos"], getLabels, {
+    refetchOnWindowFocus: false,
+  });
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span className="badge rounded-pill m-1 label-picker" style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}>
+        Primary
+      </span>
     </div>
-  )
-}
+  );
+};
