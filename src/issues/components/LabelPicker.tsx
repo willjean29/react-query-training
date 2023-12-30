0109ef501bb5cc8c8ddedc57125c844e062eddb1
Jean Osco <willjean29@gmail.com>
2023-12-30 15:44:54 -0500

    feat(issues): add uselabel custom hooks

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,19 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
-import { githubApi } from "../../api/github.api";
-import { Label } from "../interfaces/label";
-const getLabels = async (): Promise<Label[]> => {
-  const { data } = await githubApi.get<Label[]>("/labels");
-  return data;
-};
+import { useLabels } from "../hooks/useLabels";
+
 export const LabelPicker = () => {
-  const labelsQuery = useQuery(["todos"], getLabels, {
-    refetchOnWindowFocus: false,
-  });
+  const { labelsQuery } = useLabels();
+  if (labelsQuery.isLoading) {
+    return <h1>Loading ...</h1>;
+  }
   return (
     <div>
-      <span className="badge rounded-pill m-1 label-picker" style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}>
-        Primary
-      </span>
+      {labelsQuery.data?.map((label) => (
+        <span
+          key={label.id}
+          className="badge rounded-pill m-1 label-picker"
+          style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
+        >
+          {label.name}
+        </span>
+      ))}
     </div>
   );
 };
---
+++ src/issues/hooks/useLabels.ts
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+import { githubApi } from "../../api/github.api";
+import { Label } from "../interfaces/label";
+import { sleep } from "../../helpers/sleep";
+
+const getLabels = async (): Promise<Label[]> => {
+  await sleep(2);
+  const { data } = await githubApi.get<Label[]>("/labels");
+  return data;
+};
+
+export const useLabels = () => {
+  const labelsQuery = useQuery(["labels"], getLabels, {
+    staleTime: 1000 * 60 * 60,
+  });
+
+  return { labelsQuery };
+};
